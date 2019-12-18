9b4ec5fbf4998e1748b6a8b5ec431bc90b0238cd
L4ur3nsC <laurens.hamm@hamm.nu>
2019-12-18 10:11:21 +0100

    Checked commenting

--- blockchain.py
+++ blockchain.py
@@ -1,24 +1,26 @@
-#Creation of backend
+# Creation of back-end mechanics.
 import json
 import os
 import hashlib
 
-#After creating first block by hand, we wrote create_block function to create the following blocks in the blockchain. The function uses two more functions get_blocks and create_hash, which are defined below.
+
+# After creating the first block (genesis block) by hand, we wrote create_block() function to create new blocks in
+# the blockchain. The function uses two more functions get_blocks() and create_hash(), which are defined below.
 def create_block(name, birthday, subject, graduation, key):
-    #set directory to the blocks directory
+    # Set directory to the blocks directory. If not already set.
     try:
         os.chdir(os.curdir + "/blocks")
     except:
         pass
-    #conduct get_block function, which is defined next
+    # Call get_block() function, which is defined below to retrieve list of blocks.
     blocks = get_blocks()
-    #as starting block has the name zero, last block's name will always be number of blocks -1     
+    # As starting block is named zero ("0"), last block's name will be number of (blocks - 1).
     last_block = len(blocks) - 1
-    #number of new block must be last blocks's number +1
+    # Number of new block must be last blocks's number + 1. This needs to be a string as we use it as the name for new block.
     new_block = str(last_block + 1)
 
     previous_hash = create_hash(str(last_block))
-    #Definition data in each block    
+    # Definition of the data structure in each block.
     data = {
                 'name': name,
                 'date of birth': birthday,
@@ -26,22 +28,23 @@ def create_block(name, birthday, subject, graduation, key):
                 'date of graduation': graduation,
                 'hash': previous_hash
             }
-    #With statement to make a new file for new blocks using predefined new_block number as name
+    # With statement to make a new file for new blocks using predefined new_block number as name and data as data.
     with open(new_block + ".txt", 'w') as txt_file:
         json.dump(data, txt_file, indent=4)
 
-#Function, which sorts blocks and therefore enables us to number blocks in a consecutive number
+
+# Function get_blocks(), which reads and sorts blocks and therefore enables us to number blocks in a consecutive manner.
 def get_blocks():
-    #set directory to the blocks directory
+    # Set directory to the blocks directory. If not already set.
     try:
         os.chdir(os.curdir + "/blocks")
     except:
         pass
-    #Use of listdir to get all the blocks in our blockchain directory
+    # Use of listdir to get all the blocks in our blocks directory.
     blocks = os.listdir()
-    #As listdir does not sort blocks, we need to sort all blocks to get the newest one by using list comprehension to return integers for all file names and sort them.
-    blocks = sorted(blocks)
 
+    # As listdir does not sort blocks, we need to sort all blocks to get them in 'chronological' order. We add them
+    # to a list as integers (hence, we remove the file extensions).
     sorted_blocks = []
 
     for i in range(0, len(blocks)):
@@ -54,29 +57,32 @@ def get_blocks():
     sorted_blocks.sort()
     return sorted_blocks
 
-#Function to create hash for block, by using hashlib module. 
+
+# Function to create hash for the data in each block, by using hashlib module.
 def create_hash(block_name):
-    #Function reads the contents of a block and runs them through the SHA-256 hashing algorithm and returns a hash.
+    # Function reads the contents of a block and runs them through the SHA-256 hashing algorithm and returns this hash.
     block = open(block_name + ".txt", 'rb')
     block_content = block.read()
     block.close()
     return hashlib.sha256(block_content).hexdigest()
 
-#Function to verify integrity of chain. It does that by verifying the hashes of each block
+
+# Function to verify the integrity of the blockchain. It does so by verifying the hashes of each block's surrounding blocks.
 def verify_chain():
-    #Declaring variable blocks that uses predefined function get_blocks() to get all blocks in a sorted order
+    # Declaring variable blocks that is the output of the previously defined function get_blocks() to get all blocks in a sorted order.
     blocks = get_blocks()
     results = []
-    print(blocks)
-    #For loop to iterate through the blocks and read previous blocks' hashes. 
+    # For-loop to iterate through the blocks and read previous and next blocks' hashes and compare them.
+
     for block in blocks[2:]:
         previous_hash = json.load(open(str(block) + ".txt"))['hash']
         previous_block = str(int(block)-1)
-        #Control hash is created - the control hash is implemented by pretending to create a new hash for given block
+        # Control hash is created - the control hash is implemented by pretending to create a new hash for given block's data.
         true_previous_hash = create_hash(previous_block)
-        #If given hash in previous block is equal to controll hash, current hash will be checked, otherwise current block corrupted as previous hash is also corrupted 
+        # If given hash in previous block is equal to control hash, current block content will be compared with next block hash,
+        # otherwise current block is corrupted.
         if previous_hash == true_previous_hash:
-            #Same control procedure for current hash, same outcome wether hash is corrupted or not
+            # Same control procedure for current block content and next block's hash.
             try:
                 next_block = str(int(block) + 1)
                 current_hash = create_hash(block)
@@ -85,33 +91,39 @@ def verify_chain():
                     result = "a Genuine Block"
                 else:
                     result = "a Corrupted Block"
+            # If the current block is the last block in the chain, there is no next block, hence the try... except... construction.
             except:
                 result = "a Genuine Block"
         else:
             result = "a Corrupted Block"
 
+        # Each result is added to a list of tuples in combination with the number of the block.
         results.append({'block': previous_block, 'result': result})
 
     results.append({'block': len(blocks) - 1, 'result': "the last bock in the chain: unverifiable"})
-
     return results
 
-#Function to check if there is a block, which containts data entered by user
+
+# Function to check if there is a block, which contains the data entered by user on Verify Diploma page.
 def verify_diploma(name, birthday, subject, graduation):
+    # Declaring variable blocks that is the output of the previously defined function get_blocks() to get all blocks in a sorted order.
     blocks = get_blocks()
     results = []
     result = "This is a fake diploma!"
-    #For loop to open blocks and load respective data
+    # For-loop to open blocks and load respective data.
     for block in blocks[1:]:
         previous_hash = json.load(open(str(block) + ".txt"))['hash']
         b_name = json.load(open(str(block) + ".txt"))['name']
         b_birthday = json.load(open(str(block) + ".txt"))['date of birth']
         b_subject = json.load(open(str(block) + ".txt"))['study program']
         b_graduation = json.load(open(str(block) + ".txt"))['date of graduation']
-        #If statement to compare input of user with data in each block. If one block contains all entered information output to user: genuine diploma, otherwise output: fake diploma
+
+        # If statement to compare input of user with data in each block. If one block contains all entered information,
+        # we will check whether it is a genuine block. If so, output to user: genuine diploma, otherwise: fake diploma.
         if b_name == name and b_birthday == birthday and b_subject == subject and b_graduation == graduation:
             previous_block = str(int(block) - 1)
             true_previous_hash = create_hash(previous_block)
+            # Same verification procedure as in verify_chain() method above.
             if previous_hash == true_previous_hash:
                 try:
                     next_block = str(int(block) + 1)
@@ -132,4 +144,3 @@ def verify_diploma(name, birthday, subject, graduation):
 
     results.append(result)
     return results
-
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-#Creation of front end by using Python's Flask structure
+# Creation of front-end mechanics by using Python's Flask extension.
 import blockchain as bc
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import url_for
 from flask import redirect
-#Link to file template to enable Flask to comprehend paths
+
+# Link to file template to enable Flask to comprehend paths.
 app = Flask(__name__, template_folder='template')
 
-#Routing to index.html page, which is the base page of our application
+# Routing to index.html page, which is the home page of our application.
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-#Routing to addgraduate.html, where function add_graduate enables user to fill form to add a new block to blockchain
+# Routing to addgraduate.html, where function add_graduate() enables user to fill form to add a new block to blockchain.
 @app.route('/addgraduate', methods=['POST', 'GET'])
 def add_graduate():
     if request.method == 'POST':
@@ -22,12 +23,15 @@ def add_graduate():
         birthday = request.form['birthday']
         subject = request.form['subject']
         graduation = request.form['graduation']
-        #Checks for input
+        # Check if user input is correctedly filled in and if the key is correct. Otherwise, produce negative feedback.
         if len(name) == 0 or subject == "Choose..." or birthday == "" or graduation == "" or key != "12345":
             feedback = "The input is invalid. Please try again."
             return render_template('addgraduate.html', feedback=feedback, subject=subject, color="red", name=name,
                                    birthday=birthday, graduation=graduation, key=key)
-        #If all input valid, it returns success message and uses create_block() function from blockchain.py to add a new block to blockcahin
+
+        # If all input fields are valid, we return a success message and call the create_block() function from
+        # blockchain.py to add a new block to blockchain with the data provided by the user. After successfully
+        # creating a block, we reset all input fields.
         feedback = "Diploma successfully added to the blockchain!"
         bc.create_block(name=name, birthday=birthday, subject=subject, graduation=graduation, key=key)
         return render_template('addgraduate.html', feedback=feedback, subject="Choose...", color="green", name="",
@@ -36,7 +40,8 @@ def add_graduate():
     return render_template('addgraduate.html', subject="Choose...", color="green", name="",
                                    birthday="", graduation="", key="")
 
-#Routing to chechkgraduate.html, where user can fill a form to check whether block with entered data already exisits
+# Routing to checkgraduate.html, where the user can fill out a form to check whether a block with entered data exists in
+# the chain and whether this block is genuine or corrupted.
 @app.route('/checkgraduate', methods=['POST', 'GET'])
 def check_graduate():
     subject = "Choose..."
@@ -45,28 +50,33 @@ def check_graduate():
         birthday = request.form['birthday']
         subject = request.form['subject']
         graduation = request.form['graduation']
-        #Checks if all input-fiels are filled and returns error message if not
+        # Checks if all input-fiels are filled in correctly and returns feedback message if not.
         if len(name) == 0 or subject == "Choose..." or birthday == "" or graduation == "":
             feedback = "The input is invalid. Please try again."
             return render_template('checkgraduate.html', feedback=feedback, subject=subject)
-        #If form succesfully filled verify_diploma() function from blockchain.py will search each block in chain for entered data and return false block or geniune block 
+        # If the form is successfully filled in, the verify_diploma() function from blockchain.py will search through
+        # each block in the chain for entered data and if its real.
         result = bc.verify_diploma(name=name, birthday=birthday, subject=subject, graduation=graduation)
-        #User will be routed to diploma.html (function is defined below), where result of verify_diploma will be shown. 
+        # User will be routed to diploma.html, where result of verify_diploma() method will be shown.
         return diploma(result)
 
     return render_template('checkgraduate.html', subject=subject)
 
-#Routing to verification.html, where without any input from user function verify_chain from blockchain.py will be executed and check if blockchain is corrupted
+# Routing to verification.html, where without any input from user the method verify_chain() from blockchain.py will be
+# executed before entering the page and the output will be shown.
 @app.route('/verification', methods=['GET'])
 def verification():
     results = bc.verify_chain()
     return render_template('verification.html', results=results)
 
 
+# Simple method that loads the result-page of the method verify_diploma().
 @app.route('/diploma', methods=['GET'])
 def diploma(result):
     return render_template('diploma.html', results=result)
 
-#Standard statement that checks if the script is launched from the console and runs our application. Debug is added to allow flask to relaid everytime if code is changed
+
+# Standard statement that checks if the script is launched from the console and that runs our application if not.
+# Debug is added to allow flask to reload every time if code is changed.
 if __name__ == '__main__':
     app.run(debug=True)
